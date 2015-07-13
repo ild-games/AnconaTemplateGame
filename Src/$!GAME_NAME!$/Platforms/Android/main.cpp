424a091e40092fd8421e0dd26909bcac454988b7
tlein <tlein@radford.edu>
2015-07-13 00:50:58 -0500

    fix android main

--- Src/$!GAME_NAME!$/Platforms/Android/main.cpp
+++ Src/$!GAME_NAME!$/Platforms/Android/main.cpp
@@ -1,4 +1,6 @@
 #include <Ancona/Framework/Config/Config.hpp>
+#include <Ancona/System/Android/AndroidPlatform.hpp>
+#include <Ancona/System/Platform.hpp>
 
 #include "../../$!GAME_ABBR!$Lib/Core/$!GAME_ABBR!$Game.hpp"
 #include "../../$!GAME_ABBR!$Lib/Core/$!GAME_ABBR!$AndroidFactory.hpp"
@@ -9,7 +11,9 @@ int main(int argc, const char *argv[])
 {
     PolymorphicRegistration::RegisterType<SpriteDrawable>("ild::SpriteDrawable");
 
-    Config::Load("Config.txt");
+    AndroidPlatform::assetManager((AAssetManager *) argv);
+
+    Config::Load(*Platform::GetInputFileStream("Config.txt"));
 
     $!GAME_ABBR!$Game game(800, 600, new $!GAME_ABBR!$AndroidFactory());
     game.Run();
